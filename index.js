b58a3d988a750dad54c6e79ef7ba7f05cbf9b5ab
onhope <chch4934@gmail.com>
2023-12-18 18:44:13 +0900

    Random Color Generator

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ generateColors();
 
 function generateColors() {
   colorContainerEls.forEach((colorContainerEl) => {
+    // colorCode를 newColorCode에 저장   
     const newColorCode = randomColor();
     // console.log(newColorCode);
     colorContainerEl.style.backgroundColor = "#" + newColorCode;
@@ -25,8 +26,11 @@ function randomColor() {
   const chars = "0123456789abscdef";
   const colorCodeLength = 6;
   let colorCode = "";
+  // colorCodeLength의 길이만큼 반복을 함 (인덱스 0~5)
   for (let index = 0; index < colorCodeLength; index++) {
+    //chars의 길이만큼 랜덤으로 숫자를 생성
     const randomNum = Math.floor(Math.random() * chars.length);
+    // 램덤생성숫자~ 램덤생성숫자+1 만큼 substring으로 chars의 문자열을 자르고 colorcode에 +
     colorCode += chars.substring(randomNum, randomNum + 1);
   }
   return colorCode;
